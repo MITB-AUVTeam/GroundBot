e3bea1fc1942de9acb23263c0fc4fd9405fde438
Azad Roy <jovianarsenic@gmail.com>
2025-05-30 12:46:05 +0530

    PWM added

--- esp32_motor_test/esp32_motor_test.ino
+++ esp32_motor_test/esp32_motor_test.ino
@@ -1,31 +1,55 @@
 #include <Wire.h>
 
+// Motor control pins
 int m2i1 = 26;
 int m2i2 = 25;
-int m2e = 27;
+int m2e  = 27;
 int m1i1 = 12;
 int m1i2 = 14;
-int m1e = 13;  
+int m1e  = 13;
+
+const int pwmFreq = 5000;
+const int pwmResolution = 8;
 
 void setup() {
   pinMode(m1i1, OUTPUT);
   pinMode(m1i2, OUTPUT);
-  pinMode(m1e, OUTPUT);
   pinMode(m2i1, OUTPUT);
   pinMode(m2i2, OUTPUT);
-  pinMode(m2e, OUTPUT);
+
+  // Initialize PWM on motor enable pins
+  ledcAttach(m1e, pwmFreq, pwmResolution);
+  ledcAttach(m2e, pwmFreq, pwmResolution);
 
   Serial.begin(9600);
 }
 
 void loop() {
-
+  // Set motor directions
   digitalWrite(m2i1, LOW);
   digitalWrite(m2i2, HIGH);
   digitalWrite(m1i1, LOW);
   digitalWrite(m1i2, HIGH);
 
-  analogWrite(m1e, 255);
-  analogWrite(m2e, 255);
+  // Run motors at full speed (255 max for 8-bit resolution)
+
+  for(int i = 0; i <= 255; i++)
+  {
+    ledcWrite(m1e, i);
+    ledcWrite(m2e, i);
+
+    delay(20);  // Run motors for 2 seconds
+  }
+
+  delay(500);
+
+  for(int i = 255; i >= 0; i--)
+  {
+    ledcWrite(m1e, i);
+    ledcWrite(m2e, i);
+
+    delay(20);  // Run motors for 2 seconds
+  }
   
+  delay(500);
 }
